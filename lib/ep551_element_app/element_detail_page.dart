3906bb6f9a681b070e3d3bd6356708a48e14ab0b
Dreamwalker <aristojeff@gmail.com>
2020-08-22 19:18:40 +0900

    Create element_detail_page.dart

---
+++ lib/ep551_element_app/element_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ElementDetailPage extends StatefulWidget {
+  @override
+  _ElementDetailPageState createState() => _ElementDetailPageState();
+}
+
+class _ElementDetailPageState extends State<ElementDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
