b3c1dc8a7c75d0409ee1d23e97e0659206db6729
Dreamwalker <aristojeff@gmail.com>
2020-08-22 19:12:30 +0900

    Create element_main_page.dart

---
+++ lib/ep551_element_app/element_main_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class ChemicalElementApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
