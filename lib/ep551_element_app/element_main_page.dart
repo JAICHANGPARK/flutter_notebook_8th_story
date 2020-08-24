4ee7694785d31ac1c9c1bb5c178dea6c921d67d6
Dreamwalker <aristojeff@gmail.com>
2020-08-24 19:52:13 +0900

    Update element_main_page.dart

--- lib/ep551_element_app/element_main_page.dart
+++ lib/ep551_element_app/element_main_page.dart
@@ -142,6 +142,7 @@ class _ElementMainPageState extends State<ElementMainPage> {
                                     margin: EdgeInsets.symmetric(horizontal: 16),
                                     decoration: BoxDecoration(color: Colors.red,
                                     borderRadius: BorderRadius.circular(24)),
+                                    padding: EdgeInsets.all(16),
                                     child: Column(
                                       crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
