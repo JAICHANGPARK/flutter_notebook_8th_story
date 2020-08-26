c63199c106e4ab734dac0203f56c3d041a76bfaf
Dreamwalker <aristojeff@gmail.com>
2020-08-26 20:42:45 +0900

    Update element_detail_page.dart

--- lib/ep551_element_app/element_detail_page.dart
+++ lib/ep551_element_app/element_detail_page.dart
@@ -11,8 +11,9 @@ class _ElementDetailPageState extends State<ElementDetailPage> {
     return Scaffold(
       body: Stack(
         children: [Positioned(left: 0, right: 0, top: 0, bottom: 0, child: Hero(
-
           tag: "image",
+          child: Image.network("https://cdn.pixabay.com/photo/2017/08/25/18/48/art-2681039_960_720.jpg",
+          fit: BoxFit.cover,),
         ))],
       ),
     );
