a7c27292c9b8cc4e57559ec6d1042d98e1c545d8
Dreamwalker <aristojeff@gmail.com>
2020-08-26 20:39:12 +0900

    Update element_detail_page.dart

--- lib/ep551_element_app/element_detail_page.dart
+++ lib/ep551_element_app/element_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ElementDetailPage extends StatefulWidget {
   @override
   _ElementDetailPageState createState() => _ElementDetailPageState();
@@ -10,7 +9,12 @@ class _ElementDetailPageState extends State<ElementDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [Positioned(left: 0, right: 0, top: 0, bottom: 0, child: Hero(
 
+          tag: "image",
+        ))],
+      ),
     );
   }
 }
