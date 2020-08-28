14e2424a5bca40fcec91f33ed93c13d46d6b8304
Dreamwalker <aristojeff@gmail.com>
2020-08-28 20:28:01 +0900

    Update medial_app.dart

--- lib/ep557_medical_app/medial_app.dart
+++ lib/ep557_medical_app/medial_app.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class MedicalApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MedicalHomePage(),
+    );
+  }
+}
