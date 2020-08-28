b2951392715e3fe60e40f9a0325b179ad0c151c1
Dreamwalker <aristojeff@gmail.com>
2020-08-28 20:29:28 +0900

    Update medical_home_page.dart

--- lib/ep557_medical_app/medical_home_page.dart
+++ lib/ep557_medical_app/medical_home_page.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class MedicalHomePage extends StatefulWidget {
+  @override
+  _MedicalHomePageState createState() => _MedicalHomePageState();
+}
+
+class _MedicalHomePageState extends State<MedicalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
