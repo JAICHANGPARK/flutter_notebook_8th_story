2833b196ea3088ec8ab318fc413157334507308e
Dreamwalker <aristojeff@gmail.com>
2020-09-01 20:51:30 +0900

    Update fashion_store_page.dart

--- lib/ep561_fashion_store_app/fashion_store_page.dart
+++ lib/ep561_fashion_store_app/fashion_store_page.dart
@@ -23,8 +23,8 @@ class _FashionMainPageState extends State<FashionMainPage> {
           Expanded(flex: 3, child: Placeholder()),
           Expanded(flex: 2, child: Placeholder()),
           Expanded(flex: 1, child: Placeholder()),
-          Expanded(child: Placeholder()),
-          Expanded(child: Placeholder()),
+          Expanded(flex: 10,child: Placeholder()),
+          Expanded(flex: 3,child: Placeholder()),
         ],
       ),
     );
