b48975f988e8408d0988985951ff8e384691c07e
Dreamwalker <aristojeff@gmail.com>
2020-09-01 20:59:53 +0900

    Update fashion_store_page.dart

--- lib/ep561_fashion_store_app/fashion_store_page.dart
+++ lib/ep561_fashion_store_app/fashion_store_page.dart
@@ -22,60 +22,51 @@ class _FashionMainPageState extends State<FashionMainPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(flex: 4, child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){}),
-                Text("CHECKOUT", style: GoogleFonts.alata(
-                  fontWeight: FontWeight.bold
-                ),),
-                IconButton(icon: Icon(Icons.menu), onPressed: (){}),
-              ],
-            )),
-            Expanded(flex: 3, child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black,
-              ),
-            )),
-            Expanded(flex: 2, child: Container(
-              decoration: BoxDecoration(
-                color: Colors.lightGreenAccent,
-                
-              ),
-            )),
-            Expanded(flex: 15,child: ListView()),
-            Expanded(flex: 3,child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.lightGreenAccent,
-                  border: Border(
-                    top: BorderSide(color: Colors.black,
-                    width: 2)
-                  )
-              ),
-              child: Center(
-                child: Text("PROCEED TO THE NEXT STEP", style: GoogleFonts.alata(
-                  fontWeight: FontWeight.bold,
-                ),),
-              ),
-            )),
+            Expanded(
+                flex: 4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+                    Text(
+                      "CHECKOUT",
+                      style: GoogleFonts.alata(fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                  ],
+                )),
+            Expanded(
+                flex: 3,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: Colors.lightGreenAccent, border: Border.all(color: Colors.black,
+                      width: 2)),
+                )),
+            Expanded(flex: 15, child: ListView()),
+            Expanded(
+                flex: 3,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.lightGreenAccent, border: Border(top: BorderSide(color: Colors.black, width: 2))),
+                  child: Center(
+                    child: Text(
+                      "PROCEED TO THE NEXT STEP",
+                      style: GoogleFonts.alata(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
