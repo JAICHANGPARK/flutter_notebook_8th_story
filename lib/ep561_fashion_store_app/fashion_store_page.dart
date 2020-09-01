e346be349378d22754bd0adfdaa80227081495c1
Dreamwalker <aristojeff@gmail.com>
2020-09-01 20:50:52 +0900

    Update fashion_store_page.dart

--- lib/ep561_fashion_store_app/fashion_store_page.dart
+++ lib/ep561_fashion_store_app/fashion_store_page.dart
@@ -20,7 +20,11 @@ class _FashionMainPageState extends State<FashionMainPage> {
     return Scaffold(
       body: Column(
         children: [
-            Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
         ],
       ),
     );
