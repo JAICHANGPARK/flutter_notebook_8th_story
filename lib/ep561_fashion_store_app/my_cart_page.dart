f82343f24d97910fc76223ed8191d240bd1f2f5f
Dreamwalker <aristojeff@gmail.com>
2020-09-03 21:50:44 +0900

    Update my_cart_page.dart

--- lib/ep561_fashion_store_app/my_cart_page.dart
+++ lib/ep561_fashion_store_app/my_cart_page.dart
@@ -13,37 +13,40 @@ class _MyCartPageState extends State<MyCartPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(flex: 2, child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
+            Expanded(flex: 2, child: Container(
+              color: Colors.grey[400],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  children: [
 
-                  Expanded(
-                    child: Row(
-                      children: [
-                        IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){
-                          Navigator.of(context).pop();
-                        }),
-                        IconButton(icon: Icon(Icons.menu), onPressed: (){
+                    Expanded(
+                      child: Row(
+                        children: [
+                          IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){
+                            Navigator.of(context).pop();
+                          }),
+                          IconButton(icon: Icon(Icons.menu), onPressed: (){
 
-                        }),
-                      ],
-                    ),
-                  ),
-
-                  Expanded(child: Center(child: Text("MY CART"))),
-
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Badge(
-                        child: Icon(Icons.shopping_cart),
-                        badgeContent: Text("2"),
+                          }),
+                        ],
                       ),
                     ),
-                  )
 
-                ],
+                    Expanded(child: Center(child: Text("MY CART"))),
+
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Badge(
+                          child: Icon(Icons.shopping_cart),
+                          badgeContent: Text("2"),
+                        ),
+                      ),
+                    )
+
+                  ],
+                ),
               ),
             )),
             Expanded(flex: 3, child: Placeholder()),
