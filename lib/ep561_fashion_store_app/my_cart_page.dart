8bd92b9b52baa1df38ae11dd9594228be648135a
Dreamwalker <aristojeff@gmail.com>
2020-09-03 21:40:16 +0900

    Update my_cart_page.dart

--- lib/ep561_fashion_store_app/my_cart_page.dart
+++ lib/ep561_fashion_store_app/my_cart_page.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+
+class MyCartPage extends StatefulWidget {
+  @override
+  _MyCartPageState createState() => _MyCartPageState();
+}
+
+class _MyCartPageState extends State<MyCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
