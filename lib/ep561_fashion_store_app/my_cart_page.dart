211e45e97ec2167eddb88800fdea9731837981cb
Dreamwalker <aristojeff@gmail.com>
2020-09-04 21:21:25 +0900

    Update my_cart_page.dart

--- lib/ep561_fashion_store_app/my_cart_page.dart
+++ lib/ep561_fashion_store_app/my_cart_page.dart
@@ -13,84 +13,66 @@ class _MyCartPageState extends State<MyCartPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(flex: 2, child: Container(
-              color: Colors.grey[400],
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  children: [
-
-                    Expanded(
-                      child: Row(
-                        children: [
-                          IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){
-                            Navigator.of(context).pop();
-                          }),
-                          IconButton(icon: Icon(Icons.menu), onPressed: (){
-
-                          }),
-                        ],
-                      ),
-                    ),
-
-                    Expanded(child: Center(child: Text("MY CART"))),
-
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Badge(
-                          child: Icon(Icons.shopping_cart),
-                          badgeContent: Text("2"),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.grey[400],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              IconButton(
+                                  icon: Icon(Icons.arrow_back_ios),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  }),
+                              IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-
-                  ],
-                ),
-              ),
-            )),
-            Expanded(flex: 3, child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black
-              ),
-            )),
-            Expanded(flex: 18, child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[400]
-              ),
-              child: ListView.builder(
-                itemBuilder: (context, index){
-                  return Container(
-                      height: MediaQuery.of(context).size.height / 2,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200]
+                        Expanded(
+                            child: Center(
+                                child: Text(
+                          "MY CART",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ))),
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Badge(
+                              child: Icon(Icons.shopping_cart),
+                              badgeContent: Text("2"),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  );
-                },
-              ),
-            ))
+                  ),
+                )),
+            Expanded(
+                flex: 3,
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.black),
+                )),
+            Expanded(
+                flex: 18,
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.grey[400]),
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: MediaQuery.of(context).size.height / 2,
+                        decoration: BoxDecoration(color: Colors.grey[200]),
+                      );
+                    },
+                  ),
+                ))
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
