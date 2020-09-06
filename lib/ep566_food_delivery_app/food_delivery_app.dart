147fc6dc641c302051ef3b03d342ad1a79386e33
Dreamwalker <aristojeff@gmail.com>
2020-09-06 19:51:08 +0900

    Update food_delivery_app.dart

--- lib/ep566_food_delivery_app/food_delivery_app.dart
+++ lib/ep566_food_delivery_app/food_delivery_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'food_main_page.dart';
+
 class FoodDeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: "/",
       routes: {
-        "/home" : (context) =>
+        "/home" : (context) => FoodMainPage(),
       },
     );
   }
