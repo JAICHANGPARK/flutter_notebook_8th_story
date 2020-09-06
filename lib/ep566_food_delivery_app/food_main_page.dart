7714c7e63e6ba491c34daf09d456793eb7b56c00
Dreamwalker <aristojeff@gmail.com>
2020-09-06 19:51:05 +0900

    Update food_main_page.dart

--- lib/ep566_food_delivery_app/food_main_page.dart
+++ lib/ep566_food_delivery_app/food_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class FoodMainPage extends StatefulWidget {
+  @override
+  _FoodMainPageState createState() => _FoodMainPageState();
+}
+
+class _FoodMainPageState extends State<FoodMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
