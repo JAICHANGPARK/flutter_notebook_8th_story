98a6240f8cac81457715d2b03e7c770ffdc2288c
Dreamwalker <aristojeff@gmail.com>
2020-09-10 20:12:17 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 import 'ep566_food_delivery_app/food_delivery_app.dart';
 
 void main() {
-  runApp(FoodDeliveryApp());
+  runApp(SampleApp());
+}
+class SampleApp extends StatefulWidget {
+  @override
+  _SampleAppState createState() => _SampleAppState();
+}
+
+class _SampleAppState extends State<SampleApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
 }
 
