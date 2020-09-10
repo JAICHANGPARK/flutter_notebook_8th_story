3f869de57add3157526f228d8df35208cbe1468f
Dreamwalker <aristojeff@gmail.com>
2020-09-10 20:16:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'ep566_food_delivery_app/food_delivery_app.dart';
+
+final helloWorldProvider = Provider((_) => "Hello world");
 
 void main() {
   runApp(SampleApp());
