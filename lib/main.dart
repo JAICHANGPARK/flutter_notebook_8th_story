746b449800dfb71ca7808a09d2d00dd1a982a199
Dreamwalker <aristojeff@gmail.com>
2020-09-10 20:21:21 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,20 @@ import 'ep566_food_delivery_app/food_delivery_app.dart';
 final helloWorldProvider = Provider((_) => "Hello world");
 
 void main() {
-  runApp(
-    ProviderScope(child: SampleApp())
-    
-  );
+  runApp(ProviderScope(child: SampleApp()));
 }
 
-class SampleApp extends HookWidget{
+class SampleApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
+    final String value = useProvider(helloWorldProvider);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(),
 
-  );
+      ),
 
+    );
   }
-  
 }
