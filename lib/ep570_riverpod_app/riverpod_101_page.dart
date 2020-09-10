27e481617067068d04ad473c82719214bdc04b19
Dreamwalker <aristojeff@gmail.com>
2020-09-10 20:22:37 +0900

    Update riverpod_101_page.dart

---
+++ lib/ep570_riverpod_app/riverpod_101_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_notebook_8th_story/main.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+class SampleApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String value = useProvider(helloWorldProvider);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text(value),
+        ),
+
+      ),
+
+    );
+  }
+}
