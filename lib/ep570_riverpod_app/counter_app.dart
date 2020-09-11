e94acaa7031b1ab53b8da6f7ab0970623933bf80
Dreamwalker <aristojeff@gmail.com>
2020-09-11 20:47:38 +0900

    Update counter_app.dart

--- lib/ep570_riverpod_app/counter_app.dart
+++ lib/ep570_riverpod_app/counter_app.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterBody(),
+    );
+  }
+}
