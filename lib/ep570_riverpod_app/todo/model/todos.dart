9a19d381227d8914196f2e9085fda3c048953caf
Dreamwalker <aristojeff@gmail.com>
2020-09-12 19:16:54 +0900

    Update todos.dart

--- lib/ep570_riverpod_app/todo/model/todos.dart
+++ lib/ep570_riverpod_app/todo/model/todos.dart
@@ -5,11 +5,14 @@ var _uuid = Uuid();
 
 class Todo {
 
-
-  Todo({this.id, this.description, this.isCompleted = false})
-  : id = id ?? _uuid.v4();
-
   final String id;
   final String description;
   final bool isCompleted;
+
+  Todo({this.id, this.description, this.isCompleted = false});
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Todo(description: $description , completed: $isCompleted';
+  }
 }
