1dfdab02611b8c7dad1053f8c5f2ca4a2c1bec9c
Dreamwalker <aristojeff@gmail.com>
2020-09-12 19:05:38 +0900

    Update counter_app.dart

--- lib/ep570_riverpod_app/counter_app.dart
+++ lib/ep570_riverpod_app/counter_app.dart
@@ -1,8 +1,7 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_8th_story/main.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,20 +14,18 @@ class CounterApp extends StatelessWidget {
 class CounterBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Counter Riverpod"),
       ),
       body: Consumer(
-        builder: (context, value, _){
+        builder: (context, value, _) {
           final count = value(counterProvider).state;
           return Center(child: Text("Count : $count"));
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
+        onPressed: () {
           context.read(counterProvider).state++;
         },
         child: Icon(Icons.add),
