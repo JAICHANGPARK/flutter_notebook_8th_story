2a48baec8690209e244157e5049cbbc2c5dc1cb1
Dreamwalker <aristojeff@gmail.com>
2020-09-12 19:12:06 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'ep570_riverpod_app/counter_app.dart';
-import 'ep570_riverpod_app/riverpod_101_page.dart';
+import 'ep570_riverpod_app/todo/riverpod_todo_app.dart';
 
 final helloWorldProvider = Provider((_) => "Hello world");
 final counterProvider = StateProvider((ref) => 0);
 
 void main() {
-  runApp(ProviderScope(child: CounterApp()));
+  runApp(RiverPodTodoApp());
 }
 
 
