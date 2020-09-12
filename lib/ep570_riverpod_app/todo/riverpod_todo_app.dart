c0dbfd08e83b5092693908113695c3f939961b21
Dreamwalker <aristojeff@gmail.com>
2020-09-12 19:12:10 +0900

    Update riverpod_todo_app.dart

--- lib/ep570_riverpod_app/todo/riverpod_todo_app.dart
+++ lib/ep570_riverpod_app/todo/riverpod_todo_app.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class RiverPodTodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: TodoMainPage(),
+
+    );
+  }
+}
+
+
+class TodoMainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
