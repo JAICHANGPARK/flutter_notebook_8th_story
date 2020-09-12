62cd21c843e0e5049f7e3ee9656a782d108b3d96
Dreamwalker <aristojeff@gmail.com>
2020-09-12 19:23:49 +0900

    Update todo_list.dart

--- lib/ep570_riverpod_app/todo/model/todo_list.dart
+++ lib/ep570_riverpod_app/todo/model/todo_list.dart
@@ -1,19 +1,29 @@
-
 import 'package:flutter_notebook_8th_story/ep570_riverpod_app/todo/model/todos.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:state_notifier/state_notifier.dart';
 
-class TodoList extends StateNotifier<List<Todo>>{
+class TodoList extends StateNotifier<List<Todo>> {
   TodoList(List<Todo> initTodos) : super(initTodos ?? []);
 
-  void add(String value){
+  void add(String value) {
     state = [...state, Todo(description: value)];
   }
 
-  void remove(Todo value){
+  void remove(Todo value) {
     state = state.where((element) => element.id != value.id).toList();
   }
-  
 
-
+  void toggle(String id) {
+    state = [
+      for (final td in state)
+        {
+          if (td.id == id)
+            {Todo(id: td.id, isCompleted: !td.isCompleted, description: td.description)}
+          else
+            {
+              td,
+            }
+        }
+    ];
+  }
 }
