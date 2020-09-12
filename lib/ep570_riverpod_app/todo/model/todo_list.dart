20e4a2f58635be2545a4f4eab3009bb23384dedd
Dreamwalker <aristojeff@gmail.com>
2020-09-12 19:19:44 +0900

    Create todo_list.dart

---
+++ lib/ep570_riverpod_app/todo/model/todo_list.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter_notebook_8th_story/ep570_riverpod_app/todo/model/todos.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+class TodoList extends StateNotifier<List<Todo>>{
+  TodoList(List<Todo> state) : super(state ?? []);
+
+  
+
+}
