7850209ce0bd4765ba6d18385e77786d02bd1fef
Dreamwalker <aristojeff@gmail.com>
2020-09-13 19:13:47 +0900

    Update riverpod_todo_app.dart

--- lib/ep570_riverpod_app/todo/riverpod_todo_app.dart
+++ lib/ep570_riverpod_app/todo/riverpod_todo_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_notebook_8th_story/main.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class RiverPodTodoApp extends StatelessWidget {
   @override
@@ -13,8 +15,14 @@ class RiverPodTodoApp extends StatelessWidget {
 
 
 class TodoMainPage extends HookWidget {
+  const TodoMainPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+
+    final todos = useProvider(filterdTodos);
+    
+
     return Scaffold();
   }
 }
