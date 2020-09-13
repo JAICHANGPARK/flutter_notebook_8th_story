2e84d9b11470ebfe8dd49d43e5c59d2db2166d47
Dreamwalker <aristojeff@gmail.com>
2020-09-13 19:34:26 +0900

    Update todoItem.dart

--- lib/ep570_riverpod_app/todo/todoItem.dart
+++ lib/ep570_riverpod_app/todo/todoItem.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'riverpod_todo_app.dart';
 class TodoItem extends HookWidget {
   TodoItem({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final todo = useProvider(currentTodo)
+    final todo = useProvider(currentTodo);
+    final itemFocusNode = useFocusNode();
+    useListenable(itemFocusNode);
+    print(todo.id);
+
+    final isFocused = itemFocusNode.hasFocus;
+    final TextEditingController = useTextEditingController();
+    final textFocusNode = useFocusNode();
+
     throw Material(
       
     );
