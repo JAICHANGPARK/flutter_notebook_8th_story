a52b91833ff24bdb1f635a1a581160dfb315659d
Dreamwalker <aristojeff@gmail.com>
2020-09-13 19:07:32 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_8th_story/ep570_riverpod_app/todo/model/todo_list.dart';
+import 'package:flutter_notebook_8th_story/ep570_riverpod_app/todo/model/todos.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'ep570_riverpod_app/todo/riverpod_todo_app.dart';
 
@@ -10,10 +12,14 @@ final activeFilterKey = UniqueKey();
 final completedFilterKey = UniqueKey();
 final allFilterKey = UniqueKey();
 
-final todoListProvider = StateNotifierProvider((ref){});
+final todoListProvider = StateNotifierProvider((ref) {
+  return TodoList([
+    Todo(id: 'td_0', description: "Flutter"),
+    Todo(id: 'td_1', description: "Mobile"),
+    Todo(id: 'td_2', description: "Development")
+  ]);
+});
 
 void main() {
   runApp(RiverPodTodoApp());
 }
-
-
