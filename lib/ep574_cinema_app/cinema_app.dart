c2c9f9382514c16c1a7c8875f3e766d7ba82bcad
Dreamwalker <aristojeff@gmail.com>
2020-09-14 20:41:04 +0900

    Update cinema_app.dart

--- lib/ep574_cinema_app/cinema_app.dart
+++ lib/ep574_cinema_app/cinema_app.dart
@@ -17,6 +17,7 @@ class _ReservePageState extends State<ReservePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       
     );
   }
