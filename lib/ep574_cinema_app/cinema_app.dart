50bfff0dec040e10f3a602f05b0454025ac3e5a9
Dreamwalker <aristojeff@gmail.com>
2020-09-14 20:39:32 +0900

    Update cinema_app.dart

--- lib/ep574_cinema_app/cinema_app.dart
+++ lib/ep574_cinema_app/cinema_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CinemaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: ReservePage(),
+    );
   }
 }
