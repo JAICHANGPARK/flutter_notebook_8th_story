02b88e802b1fab65efe9255430757e1a5c8cffb8
Dreamwalker <aristojeff@gmail.com>
2020-09-14 20:47:55 +0900

    Update cinema_app.dart

--- lib/ep574_cinema_app/cinema_app.dart
+++ lib/ep574_cinema_app/cinema_app.dart
@@ -8,6 +8,7 @@ class CinemaApp extends StatelessWidget {
     );
   }
 }
+
 class ReservePage extends StatefulWidget {
   @override
   _ReservePageState createState() => _ReservePageState();
@@ -21,62 +22,58 @@ class _ReservePageState extends State<ReservePage> {
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
-          onPressed: (){},
+          onPressed: () {},
         ),
-        title: Text("Black Panther", style: TextStyle(
-          color: Colors.white
-        ),),
+        title: Text(
+          "Black Panther",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
             Container(
               height: MediaQuery.of(context).size.height / 7,
-              child: Placeholder(color: Colors.white,),
+              child: Placeholder(
+                color: Colors.white,
+              ),
             ),
-          Container(
-            height: MediaQuery.of(context).size.height / 3,
-            child: Placeholder(color: Colors.white,),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 10,
-            child: Placeholder(color: Colors.white,),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 10,
-            child: Placeholder(color: Colors.white,),
-          ),
-          Divider(),
-          Container(
-            height: MediaQuery.of(context).size.height / 10,
-            child: Placeholder(color: Colors.white,),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 10,
-            child: Placeholder(color: Colors.white,),
-          ),
-
-
-
-        ],
+            Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              child: Placeholder(
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 10,
+              child: Placeholder(
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 9,
+              child: Placeholder(
+                color: Colors.white,
+              ),
+            ),
+            Divider(),
+            Container(
+              height: MediaQuery.of(context).size.height / 9,
+              child: Placeholder(
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 9,
+              child: Placeholder(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
