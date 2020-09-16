9e591ce4852987471e8eb157fc35cb6efa2c149f
Dreamwalker <aristojeff@gmail.com>
2020-09-16 21:19:00 +0900

    Update chair.dart

--- lib/ep574_cinema_app/chair.dart
+++ lib/ep574_cinema_app/chair.dart
@@ -9,51 +9,43 @@ class Chair {
 }
 
 List<Chair> chairRow1 = [
-  Chair(1,1,reserveState: ReserveState.reserved),
-  Chair(1,2,reserveState: ReserveState.available),
-  Chair(1,3,reserveState: ReserveState.available),
-  Chair(1,4,reserveState: ReserveState.available),
-  Chair(1,5,reserveState: ReserveState.reserved),
-  Chair(1,6,reserveState: ReserveState.available),
-  Chair(1,7,reserveState: ReserveState.available),
-  Chair(1,8,reserveState: ReserveState.available),
+  Chair(1, 1, reserveState: ReserveState.reserved),
+  Chair(1, 2, reserveState: ReserveState.available),
+  Chair(1, 3, reserveState: ReserveState.available),
+  Chair(1, 4, reserveState: ReserveState.available),
+  Chair(1, 5, reserveState: ReserveState.reserved),
+  Chair(1, 6, reserveState: ReserveState.available),
+  Chair(1, 7, reserveState: ReserveState.available),
+  Chair(1, 8, reserveState: ReserveState.available),
 ];
 
 List<Chair> chairRow2 = [
-  Chair(2,1,reserveState: ReserveState.available),
-  Chair(2,2,reserveState: ReserveState.available),
-  Chair(2,3,reserveState: ReserveState.available),
-  Chair(2,4,reserveState: ReserveState.reserved),
-  Chair(2,5,reserveState: ReserveState.reserved),
-  Chair(2,6,reserveState: ReserveState.available),
-  Chair(2,7,reserveState: ReserveState.available),
-  Chair(2,8,reserveState: ReserveState.available),
-  Chair(2,9,reserveState: ReserveState.reserved),
-  Chair(2,10,reserveState: ReserveState.available),
+  Chair(2, 1, reserveState: ReserveState.available),
+  Chair(2, 2, reserveState: ReserveState.available),
+  Chair(2, 3, reserveState: ReserveState.available),
+  Chair(2, 4, reserveState: ReserveState.reserved),
+  Chair(2, 5, reserveState: ReserveState.reserved),
+  Chair(2, 6, reserveState: ReserveState.available),
+  Chair(2, 7, reserveState: ReserveState.available),
+  Chair(2, 8, reserveState: ReserveState.available),
+  Chair(2, 9, reserveState: ReserveState.reserved),
+  Chair(2, 10, reserveState: ReserveState.available),
 ];
 
 List<Chair> chairRow3 = [
-  Chair(2,1,reserveState: ReserveState.available),
-  Chair(2,2,reserveState: ReserveState.available),
-  Chair(2,3,reserveState: ReserveState.available),
-  Chair(2,4,reserveState: ReserveState.reserved),
-  Chair(2,5,reserveState: ReserveState.reserved),
-  Chair(2,6,reserveState: ReserveState.available),
-  Chair(2,7,reserveState: ReserveState.available),
-  Chair(2,8,reserveState: ReserveState.available),
-  Chair(2,9,reserveState: ReserveState.reserved),
-  Chair(2,10,reserveState: ReserveState.available),
+  Chair(3, 1, reserveState: ReserveState.available),
+  Chair(3, 2, reserveState: ReserveState.available),
+  Chair(3, 3, reserveState: ReserveState.available),
+  Chair(3, 4, reserveState: ReserveState.reserved),
+  Chair(3, 5, reserveState: ReserveState.available),
+  Chair(3, 6, reserveState: ReserveState.available),
+  Chair(3, 7, reserveState: ReserveState.reserved),
+  Chair(3, 8, reserveState: ReserveState.available),
+  Chair(3, 9, reserveState: ReserveState.available),
+  Chair(3, 10, reserveState: ReserveState.available),
+  Chair(3, 11, reserveState: ReserveState.available),
+  Chair(3, 12, reserveState: ReserveState.available),
 ];
-
-
-
-
-
-
-
-
-
-
 
 
 
