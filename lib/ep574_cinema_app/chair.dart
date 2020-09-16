806ac11d96e584c1db871cae92cc9a99f0c114db
Dreamwalker <aristojeff@gmail.com>
2020-09-16 21:15:46 +0900

    Update chair.dart

--- lib/ep574_cinema_app/chair.dart
+++ lib/ep574_cinema_app/chair.dart
@@ -7,3 +7,14 @@ class Chair {
 
   Chair(this.rowNum, this.cNum, {this.reserveState = ReserveState.available}) : assert(reserveState != null);
 }
+
+List<Chair> chairRow1 = [
+  Chair(1,1,reserveState: ReserveState.reserved),
+  Chair(1,2,reserveState: ReserveState.available),
+  Chair(1,3,reserveState: ReserveState.available),
+  Chair(1,4,reserveState: ReserveState.available),
+  Chair(1,5,reserveState: ReserveState.reserved),
+  Chair(1,6,reserveState: ReserveState.available),
+  Chair(1,7,reserveState: ReserveState.available),
+  Chair(1,8,reserveState: ReserveState.available),
+];
