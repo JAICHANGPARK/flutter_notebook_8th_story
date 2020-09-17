1b8def481ab2b2a2dce2156c3a0b274f3503fd35
Dreamwalker <aristojeff@gmail.com>
2020-09-17 20:30:20 +0900

    Update cinema_provider.dart

--- lib/ep574_cinema_app/cinema_provider.dart
+++ lib/ep574_cinema_app/cinema_provider.dart
@@ -2,5 +2,12 @@ import 'package:flutter/foundation.dart';
 
 class CinemaProvider with ChangeNotifier{
 
+  String _rowSelected = "";
+
+  String get rowSelected => _rowSelected;
+  void setRowSelected(String s){
+    _rowSelected = s;
+    notifyListeners();
+  }
 
 }
