2c2a13f6390e48b1d091ea25a8b300027e095c23
Dreamwalker <aristojeff@gmail.com>
2020-09-17 20:41:50 +0900

    Update cinema_provider.dart

--- lib/ep574_cinema_app/cinema_provider.dart
+++ lib/ep574_cinema_app/cinema_provider.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_notebook_8th_story/ep574_cinema_app/chair.dart';
 
-class CinemaProvider with ChangeNotifier{
-
+class CinemaProvider with ChangeNotifier {
   String _rowSelected = "";
-  List<Chair> _columnSelected = Map<int, int>();
+  List<Chair> _columnSelected = [];
 
   String get rowSelected => _rowSelected;
-  void setRowSelected(String s){
+
+  void setRowSelected(String s) {
     _rowSelected = s;
     notifyListeners();
   }
 
-  Map<int, int> get columnSelected => _columnSelected;
+  List<Chair> get columnSelected => _columnSelected;
 
-  void addSelectedChair(int row){
-    _columnSelected[] = ;
-  }
+  void addSelectedChair(int row) {}
 
-  void removeSelectedChair(int column){
-
-  }
-
+  void removeSelectedChair(int column) {}
 }
