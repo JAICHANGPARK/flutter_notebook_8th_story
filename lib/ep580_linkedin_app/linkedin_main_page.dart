df55d7a4ee1da2e1cdb96620f409a3a020969472
Dreamwalker <aristojeff@gmail.com>
2020-09-20 20:10:44 +0900

    Update linkedin_main_page.dart

--- lib/ep580_linkedin_app/linkedin_main_page.dart
+++ lib/ep580_linkedin_app/linkedin_main_page.dart
@@ -19,40 +19,46 @@ class _LinkedinMainPageState extends State<LinkedinMainPage> {
                 stops: [0.0, 0.4, 0.8],
                 colors: [Colors.blue[100], Colors.blue[50], Colors.blue[50]])),
         child: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Row(
-                  children: [
-                    CircleAvatar(),
-                    Expanded(
-                      flex: 6,
-                      child: Card(
-                        child: TextField(),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    children: [
+                      CircleAvatar(),
+                      Expanded(
+                        flex: 6,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            child: TextField(),
+                          ),
+                        ),
                       ),
-                    ),
-                    Expanded(child: Card(
-                      child: IconButton(icon: Icon(Icons.chat_bubble_outline),
-                      onPressed: (){},),
-                    )),
-                  ],
+                      Expanded(child: Card(
+                        child: IconButton(icon: Icon(Icons.chat_bubble_outline),
+                        onPressed: (){},),
+                      )),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Placeholder(),
-              ),
-              Expanded(
-                flex: 15,
-                child: Placeholder(),
-              ),
-              Expanded(
-                flex: 2,
-                child: Placeholder(),
-              ),
+                Expanded(
+                  flex: 3,
+                  child: Placeholder(),
+                ),
+                Expanded(
+                  flex: 15,
+                  child: Placeholder(),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Placeholder(),
+                ),
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
