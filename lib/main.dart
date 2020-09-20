832274871eac977847488a5cac58dd6176b42595
Dreamwalker <aristojeff@gmail.com>
2020-09-20 20:01:03 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_8th_story/ep570_riverpod_app/todo/model/todo_list.dart';
 import 'package:flutter_notebook_8th_story/ep570_riverpod_app/todo/model/todos.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'ep570_riverpod_app/todo/riverpod_todo_app.dart';
-import 'ep574_cinema_app/cinema_app.dart';
-import 'ep578_dental_service/dental_service_app.dart';
 import 'ep580_linkedin_app/linkedin_app.dart';
 
 enum TodoListFilter{
