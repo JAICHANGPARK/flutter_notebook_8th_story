3e11bfc27b433ed8e76f23dada45bd82ec07576b
Dreamwalker <aristojeff@gmail.com>
2020-09-20 20:00:31 +0900

    Update linkedin_app.dart

--- lib/ep580_linkedin_app/linkedin_app.dart
+++ lib/ep580_linkedin_app/linkedin_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LinkedinApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: LinkedinMainPage(),
+    );
   }
 }
