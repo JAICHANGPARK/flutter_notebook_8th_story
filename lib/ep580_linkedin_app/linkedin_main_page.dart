d869b572f67a3df11fa7263cc8526f5ad143cbf9
Dreamwalker <aristojeff@gmail.com>
2020-09-20 20:12:15 +0900

    Update linkedin_main_page.dart

--- lib/ep580_linkedin_app/linkedin_main_page.dart
+++ lib/ep580_linkedin_app/linkedin_main_page.dart
@@ -9,6 +9,8 @@ class _LinkedinMainPageState extends State<LinkedinMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
