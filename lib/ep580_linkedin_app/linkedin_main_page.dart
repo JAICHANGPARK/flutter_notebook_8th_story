879b5da44c048be74390113caecb88e13d4af595
Dreamwalker <aristojeff@gmail.com>
2020-09-20 20:01:35 +0900

    Update linkedin_main_page.dart

--- lib/ep580_linkedin_app/linkedin_main_page.dart
+++ lib/ep580_linkedin_app/linkedin_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class LinkedinMainPage extends StatefulWidget {
+  @override
+  _LinkedinMainPageState createState() => _LinkedinMainPageState();
+}
+
+class _LinkedinMainPageState extends State<LinkedinMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
