18a7950a39d8ddbfd2154c78c9cc8b0c459e7898
Dreamwalker <aristojeff@gmail.com>
2020-09-23 19:54:58 +0900

    Update linkedin_main_page.dart

--- lib/ep580_linkedin_app/linkedin_main_page.dart
+++ lib/ep580_linkedin_app/linkedin_main_page.dart
@@ -33,8 +33,12 @@ class _LinkedinMainPageState extends State<LinkedinMainPage> {
                   flex: 2,
                   child: Row(
                     children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage("https://avatars0.githubusercontent.com/u/19484515?s=460&u=0ec7b31ff9129826cccc5cd971887a9dd0e0a538&v=4"),
+                      GestureDetector(
+                        onTap: (){},
+                        child: CircleAvatar(
+
+                          backgroundImage: NetworkImage("https://avatars0.githubusercontent.com/u/19484515?s=460&u=0ec7b31ff9129826cccc5cd971887a9dd0e0a538&v=4"),
+                        ),
                       ),
                       Expanded(
                         flex: 9,
