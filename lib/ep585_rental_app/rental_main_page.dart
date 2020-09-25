29c020f917d54a39d23fd3dfda5797510ff4f5eb
Dreamwalker <aristojeff@gmail.com>
2020-09-25 20:22:29 +0900

    Create rental_main_page.dart

---
+++ lib/ep585_rental_app/rental_main_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class RentalMainPage extends StatefulWidget {
+  @override
+  _RentalMainPageState createState() => _RentalMainPageState();
+}
+
+class _RentalMainPageState extends State<RentalMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
