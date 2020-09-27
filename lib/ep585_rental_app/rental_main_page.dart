9b6031d73b0ad389937543e52dfa67fb0926b282
Dreamwalker <aristojeff@gmail.com>
2020-09-27 18:30:55 +0900

    Update rental_main_page.dart

--- lib/ep585_rental_app/rental_main_page.dart
+++ lib/ep585_rental_app/rental_main_page.dart
@@ -153,7 +153,9 @@ class _RentalMainPageState extends State<RentalMainPage> {
                                   child: Container(
                                     decoration: BoxDecoration(
                                       image: DecorationImage(
-                                        image: NetworkImage("")
+                                        image: NetworkImage("https://cdn.pixabay.com/photo/2017/03/22/17/39/kitchen-2165756_960_720.jpg",
+                                        ),
+                                        fit: BoxFit.cover
                                       )
                                     ),
                                   ),
