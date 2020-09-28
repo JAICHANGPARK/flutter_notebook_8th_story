93573c8af9dba4329a93b19fa7ae9dc79e91f755
Dreamwalker <aristojeff@gmail.com>
2020-09-28 19:02:03 +0900

    Create social_home_page.dart

---
+++ lib/ep588_social_app/social_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SocialHomePage extends StatefulWidget {
+  @override
+  _SocialHomePageState createState() => _SocialHomePageState();
+}
+
+class _SocialHomePageState extends State<SocialHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
