36c0337a6e88d5b2a1602b6b74d39dde6060fe41
Dreamwalker <aristojeff@gmail.com>
2020-09-28 19:05:22 +0900

    Update social_enter_page.dart

--- lib/ep588_social_app/social_enter_page.dart
+++ lib/ep588_social_app/social_enter_page.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class SocialEnterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Text("New Notification(12)", style: TextStyle(
-            color: Colors.white,
-          ),),
-          Container(
-            height: 120,
-            width: double.infinity,
-            decoration: BoxDecoration(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("New Notification(12)", style: TextStyle(
               color: Colors.white,
+            ),),
+            Container(
+              height: 120,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
             ),
-          )
-        ],
+            Text("Solve Doubts",style: GoogleFonts.montserrat(
+              color: Colors.white,
+              fontSize: 20,
+            ),),
+            Text("Join Millions of Squads From Around World, learning On Chat!",
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.white
+            ),)
+          ],
+        ),
       ),
     );
   }
