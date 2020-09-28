ee979741d24eddf614d0458060b6cca538c097cd
Dreamwalker <aristojeff@gmail.com>
2020-09-28 19:03:27 +0900

    Update social_enter_page.dart

--- lib/ep588_social_app/social_enter_page.dart
+++ lib/ep588_social_app/social_enter_page.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 class SocialEnterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Text("New Notification(12)", style: TextStyle(
+            color: Colors.white,
+          ),)
+        ],
+      ),
+    );
   }
 }
