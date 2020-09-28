a89599e0cd072d42f72c2814c419fd83201dd27d
Dreamwalker <aristojeff@gmail.com>
2020-09-28 19:03:28 +0900

    Update social_app.dart

--- lib/ep588_social_app/social_app.dart
+++ lib/ep588_social_app/social_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'social_enter_page.dart';
+import 'social_home_page.dart';
 
 
 class SocialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: "/",
       routes: {
         "/": (context) => SocialEnterPage(),
         "/home" :(context) => SocialHomePage(),
