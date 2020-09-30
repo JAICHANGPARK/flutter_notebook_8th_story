92dc5656d9f4bf8bba5a92af2291be595e13ef97
Dreamwalker <aristojeff@gmail.com>
2020-09-30 18:50:23 +0900

    Update social_enter_page.dart

--- lib/ep588_social_app/social_enter_page.dart
+++ lib/ep588_social_app/social_enter_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class SocialEnterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,9 +9,12 @@ class SocialEnterPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Text("New Notification (12)", style: GoogleFonts.montserrat(
-              color: Colors.white.withOpacity(0.8),
-            ),),
+            Text(
+              "New Notification (12)",
+              style: GoogleFonts.montserrat(
+                color: Colors.white.withOpacity(0.8),
+              ),
+            ),
             Spacer(),
             Container(
               height: 200,
@@ -18,60 +22,47 @@ class SocialEnterPage extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Colors.white,
               ),
-            ),Spacer(),
-            Text("Solve Doubts",style: GoogleFonts.montserrat(
-              color: Colors.white,
-              fontSize: 32,
-              fontWeight: FontWeight.bold
-            ),),
-            SizedBox(height: 32,),
-            Text("Join Millions of Squads From Around "
-                "\nWorld, learning On Chat!",
-            textAlign: TextAlign.center,
-            style: GoogleFonts.montserrat(
+            ),
+            Spacer(),
+            Text(
+              "Solve Doubts",
+              style: GoogleFonts.montserrat(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Text(
+              "Join Millions of Squads From Around "
+              "\nWorld, learning On Chat!",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.montserrat(
                 fontSize: 14,
                 color: Colors.white,
-              ),),
+              ),
+            ),
             Spacer(),
             GestureDetector(
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).pushNamed("/home");
               },
               child: Container(
                 height: 48,
                 width: 120,
-                decoration: BoxDecoration(
-                  color: Colors.tealAccent[100],
-                  borderRadius: BorderRadius.circular(24)
-                ),
+                decoration: BoxDecoration(color: Colors.tealAccent[100], borderRadius: BorderRadius.circular(24)),
                 child: Center(
-                  child: Text("Explore", style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.normal
-                  ),),
+                  child: Text(
+                    "Explore",
+                    style: GoogleFonts.montserrat(fontWeight: FontWeight.normal),
+                  ),
                 ),
               ),
             ),
-           SizedBox(height: 42,)
+            SizedBox(
+              height: 42,
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
