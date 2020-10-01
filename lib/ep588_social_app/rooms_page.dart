0d727af64d14c060ccc26b33e57d7d937234da5f
Dreamwalker <aristojeff@gmail.com>
2020-10-01 19:33:12 +0900

    Update rooms_page.dart

--- lib/ep588_social_app/rooms_page.dart
+++ lib/ep588_social_app/rooms_page.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+
+class RoomsPage extends StatefulWidget {
+  @override
+  _RoomsPageState createState() => _RoomsPageState();
+}
+
+class _RoomsPageState extends State<RoomsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
