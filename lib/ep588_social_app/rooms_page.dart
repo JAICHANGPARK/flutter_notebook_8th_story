78912c61695cbe6773350a6a083724d1a92e015b
Dreamwalker <aristojeff@gmail.com>
2020-10-02 18:46:39 +0900

    Update rooms_page.dart

--- lib/ep588_social_app/rooms_page.dart
+++ lib/ep588_social_app/rooms_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_8th_story/ep588_social_app/social_bottom_nav_bar.dart';
 
-
 class RoomsPage extends StatefulWidget {
   @override
   _RoomsPageState createState() => _RoomsPageState();
@@ -16,19 +15,16 @@ class _RoomsPageState extends State<RoomsPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-              Expanded(
-                flex: 2,
-                child: Placeholder(),
-              ),
             Expanded(
               flex: 2,
               child: Placeholder(),
             ),
-
-
+            Expanded(
+              flex: 10,
+              child: Placeholder(),
+            ),
           ],
         ),
-
       ),
       bottomNavigationBar: SocialBottomNavBar(),
     );
