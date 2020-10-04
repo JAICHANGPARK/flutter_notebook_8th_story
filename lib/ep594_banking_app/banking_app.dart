fe1b5f49ad3e86fbbb2b46f4cbbf066c8d1e577f
Dreamwalker <aristojeff@gmail.com>
2020-10-04 18:49:53 +0900

    Update banking_app.dart

--- lib/ep594_banking_app/banking_app.dart
+++ lib/ep594_banking_app/banking_app.dart
@@ -23,19 +23,19 @@ class BankingCardPage extends StatelessWidget {
           height: MediaQuery.of(context).size.height / 2.3,
           width: MediaQuery.of(context).size.width / 1.7,
           decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.5),
+            color: Colors.black.withOpacity(0.2),
             borderRadius: BorderRadius.circular(16),
             border: Border.all(color: Colors.grey)
           ),
           child: BackdropFilter(
             filter: ImageFilter.blur(
-              sigmaX: 2,
-              sigmaY: 2,
+              sigmaX: 4,
+              sigmaY: 4,
 
             ),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.01),
+                color: Colors.white.withOpacity(0.25),
                 border: Border.all(color: Colors.grey),
                 borderRadius: BorderRadius.circular(16)
               ),
