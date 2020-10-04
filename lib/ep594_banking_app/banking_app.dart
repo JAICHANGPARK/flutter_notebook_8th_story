827ad7702ac700488f37d08cb378ca09f195ee0f
Dreamwalker <aristojeff@gmail.com>
2020-10-04 18:58:24 +0900

    Update banking_app.dart

--- lib/ep594_banking_app/banking_app.dart
+++ lib/ep594_banking_app/banking_app.dart
@@ -23,7 +23,7 @@ class BankingCardPage extends StatelessWidget {
           height: MediaQuery.of(context).size.height / 2.3,
           width: MediaQuery.of(context).size.width / 1.7,
           decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.5),
+            color: Colors.white.withOpacity(0.8),
             borderRadius: BorderRadius.circular(16),
             border: Border.all(color: Colors.grey)
           ),
