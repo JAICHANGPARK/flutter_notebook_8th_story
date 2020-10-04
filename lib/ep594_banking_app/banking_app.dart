c4826676248108b427ce03f53ae8e56c7751ac90
Dreamwalker <aristojeff@gmail.com>
2020-10-04 18:59:49 +0900

    Update banking_app.dart

--- lib/ep594_banking_app/banking_app.dart
+++ lib/ep594_banking_app/banking_app.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-
 class BankingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,7 +10,6 @@ class BankingApp extends StatelessWidget {
     );
   }
 }
-
 
 class BankingCardPage extends StatelessWidget {
   @override
@@ -23,10 +21,9 @@ class BankingCardPage extends StatelessWidget {
           height: MediaQuery.of(context).size.height / 2.3,
           width: MediaQuery.of(context).size.width / 1.7,
           decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.8),
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: Colors.grey)
-          ),
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: Colors.grey)),
           child: BackdropFilter(
             filter: ImageFilter.blur(
               sigmaX: 2,
@@ -34,10 +31,9 @@ class BankingCardPage extends StatelessWidget {
             ),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.01),
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(16)
-              ),
+                  color: Colors.black.withOpacity(0.01),
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(16)),
               child: Stack(
                 children: [
                   Positioned(
@@ -46,36 +42,41 @@ class BankingCardPage extends StatelessWidget {
                     bottom: 48,
                     left: 100,
                     child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.pink,
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(16),
-                                topLeft: Radius.circular(16)
-                            )
-                          ),
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius:
+                              BorderRadius.only(bottomLeft: Radius.circular(16), topLeft: Radius.circular(16))),
                       padding: EdgeInsets.only(top: 16, left: 12),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("PNK", style: TextStyle(
-                            fontSize: 38
-                          ),),
-                          Text("FLUTTER PINK \$ Card", style: TextStyle(
-                            fontSize: 10
-                          ),),
-                          SizedBox(height: 24,),
-                          Text("2720",style: TextStyle(
-                            fontSize: 22
-                          ),),
-                          Text("2720",style: TextStyle(
-                              fontSize: 22
-                          ),),
-                          Text("2720",style: TextStyle(
-                              fontSize: 22
-                          ),),
-                          Text("2720",style: TextStyle(
-                              fontSize: 22
-                          ),)
+                          Text(
+                            "PNK",
+                            style: TextStyle(fontSize: 38),
+                          ),
+                          Text(
+                            "FLUTTER PINK \$ Card",
+                            style: TextStyle(fontSize: 10),
+                          ),
+                          SizedBox(
+                            height: 24,
+                          ),
+                          Text(
+                            "2720",
+                            style: TextStyle(fontSize: 22),
+                          ),
+                          Text(
+                            "2720",
+                            style: TextStyle(fontSize: 22),
+                          ),
+                          Text(
+                            "2720",
+                            style: TextStyle(fontSize: 22),
+                          ),
+                          Text(
+                            "2720",
+                            style: TextStyle(fontSize: 22),
+                          )
                         ],
                       ),
                     ),
@@ -85,7 +86,6 @@ class BankingCardPage extends StatelessWidget {
             ),
           ),
         ),
-
       ),
     );
   }
