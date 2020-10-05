88ccb5643dfa23d60303acbafa18cb7eb058f105
Dreamwalker <aristojeff@gmail.com>
2020-10-05 20:18:43 +0900

    Create bank_main_page.dart

---
+++ lib/ep594_banking_app/bank_main_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class BankMainPage extends StatefulWidget {
+  @override
+  _BankMainPageState createState() => _BankMainPageState();
+}
+
+class _BankMainPageState extends State<BankMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
