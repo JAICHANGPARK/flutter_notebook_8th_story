cfe618770dc1e9404fd0dad5b4d07be93ee3a2ae
Dreamwalker <aristojeff@gmail.com>
2020-10-05 20:20:12 +0900

    Update bank_main_page.dart

--- lib/ep594_banking_app/bank_main_page.dart
+++ lib/ep594_banking_app/bank_main_page.dart
@@ -13,7 +13,14 @@ class _BankMainPageState extends State<BankMainPage> {
       backgroundColor: Colors.black,
       body: Stack(
         children: [
-          
+          Positioned(
+            top: 32,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(),
+          )
+
         ],
       ),
     );
