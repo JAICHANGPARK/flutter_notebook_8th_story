569a11e003ac9c67100855fe4f7d08770c7a9b57
Dreamwalker <aristojeff@gmail.com>
2020-10-07 19:40:32 +0900

    Update design_app.dart

--- lib/ep597_design_app/design_app.dart
+++ lib/ep597_design_app/design_app.dart
@@ -21,7 +21,10 @@ class SplashPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-            
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
             child: Image.network(
               "https://s3.amazonaws.com/designco-web-assets/uploads/2017/07/Podcast-large_2x-1450x1112.png",
               fit: BoxFit.cover,
