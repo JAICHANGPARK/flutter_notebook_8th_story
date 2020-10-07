6bb96cceb40d0393695e10e8abad3444d3a76425
Dreamwalker <aristojeff@gmail.com>
2020-10-07 19:41:08 +0900

    Update splash_page.dart

---
+++ lib/ep597_design_app/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Image.network(
+              "https://s3.amazonaws.com/designco-web-assets/uploads/2017/07/Podcast-large_2x-1450x1112.png",
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
