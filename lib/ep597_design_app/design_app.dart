dba758115c2e20a7f2a4a7efbe81708f5489da0f
Dreamwalker <aristojeff@gmail.com>
2020-10-07 19:41:10 +0900

    Update design_app.dart

--- lib/ep597_design_app/design_app.dart
+++ lib/ep597_design_app/design_app.dart
@@ -13,25 +13,3 @@ class DesignApp extends StatelessWidget {
     );
   }
 }
-
-class SplashPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            child: Image.network(
-              "https://s3.amazonaws.com/designco-web-assets/uploads/2017/07/Podcast-large_2x-1450x1112.png",
-              fit: BoxFit.cover,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
