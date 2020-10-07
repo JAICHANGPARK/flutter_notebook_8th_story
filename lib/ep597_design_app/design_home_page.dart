eee78f8ced1f64c4c409670e197062e0e1f7febe
Dreamwalker <aristojeff@gmail.com>
2020-10-07 19:39:22 +0900

    Create design_home_page.dart

---
+++ lib/ep597_design_app/design_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class DesignHomePage extends StatefulWidget {
+  @override
+  _DesignHomePageState createState() => _DesignHomePageState();
+}
+
+class _DesignHomePageState extends State<DesignHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
