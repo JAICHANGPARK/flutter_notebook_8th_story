7dffe61be38191af2609cdf77b2a19d98a9f381a
Dreamwalker <aristojeff@gmail.com>
2020-10-08 20:31:29 +0900

    Update design_home_page.dart

--- lib/ep597_design_app/design_home_page.dart
+++ lib/ep597_design_app/design_home_page.dart
@@ -20,7 +20,8 @@ class _DesignHomePageState extends State<DesignHomePage> {
               decoration: BoxDecoration(
                   image: DecorationImage(
                       image: NetworkImage(
-                          "https://s3.amazonaws.com/designco-web-assets/uploads/2017/07/Podcast-large_2x-1450x1112.png"))),
+                          "https://s3.amazonaws.com/designco-web-assets/uploads/2017/07/Podcast-large_2x-1450x1112.png"),
+                  fit: BoxFit.cover)),
             ),
           )
         ],
