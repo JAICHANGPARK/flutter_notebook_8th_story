8ce459b6c117729a06103e412c75f3889ccc16d1
Dreamwalker <aristojeff@gmail.com>
2020-10-09 19:17:47 +0900

    Update design_book_detail.dart

--- lib/ep597_design_app/design_book_detail.dart
+++ lib/ep597_design_app/design_book_detail.dart
@@ -15,16 +15,37 @@ class _DesignBookDetailPageState extends State<DesignBookDetailPage> {
     return Scaffold(
       body: Stack(
         children: [
+
           Positioned(
             left: 0,
             right: 0,
             top: 0,
-
-            child: Container(
-              height: 320,
-              child: Placeholder(),
+            child: Hero(
+              tag: "id_img",
+              child: Container(
+                height: 320,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2019/12/12/13/42/castle-4690710_960_720.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
-          )
+          ),
+          Positioned(
+            left: 24,
+            top: 32,
+            child: IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.arrow_back_ios),
+            ),
+          ),
         ],
       ),
     );
