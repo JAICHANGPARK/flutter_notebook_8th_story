c1236b9a49b073c248d88eac3affa5d6f9f1a2f5
Dreamwalker <aristojeff@gmail.com>
2020-10-10 18:48:41 +0900

    Update design_book_detail.dart

--- lib/ep597_design_app/design_book_detail.dart
+++ lib/ep597_design_app/design_book_detail.dart
@@ -68,12 +68,22 @@ class _DesignBookDetailPageState extends State<DesignBookDetailPage> {
             ),
           ),
           Positioned(
-
+top: 320,
+            left: 16,
+            right: 16,
             child: ListView(
               children: [
-                Text("DREAM WALKER PRESENTS"),
-                Text("Principles of Product Design"),
-                Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                Center(child: Text("DREAM WALKER PRESENTS",style: TextStyle(
+                  fontSize: 12,
+                ),)),
+                Center(child: Text("Principles of Product Design", style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600
+                ),)),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                )
               ],
             ),
           )
